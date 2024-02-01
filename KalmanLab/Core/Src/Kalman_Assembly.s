8070d6b8724d975ebf00d3cd47a235cac52d2780
qinxuanx <112572108+qinxuanx@users.noreply.github.com>
2024-02-01 10:38:06 -0500

    Update Kalman_Assembly.s

--- KalmanLab/Core/Src/Kalman_Assembly.s
+++ KalmanLab/Core/Src/Kalman_Assembly.s
@@ -3,37 +3,34 @@
 .global kalman
 
 
+// states order: q, r, x, p, k
 kalman:
-    VPUSH.32 {s4, s5, s6}
+    VPUSH.32 {s4, s5, s6, s7}
 
-    // calculate p
+    // calculate p = p + q
     VLDR.32 s4, [r0, #12]     // load p in s4
     VLDR.32 s5, [r0]         // load q in S5
     VADD.F32 s4, s4, s5        // calculate p = p + q, result in s4
     VSTR.32 s4, [r0, #12]     // save new p in struct
 
-    // calculate k
+    // calculate k = p / (p + r)
     VLDR.32 s5, [r0, #4]     // load r in s5, new p already in s4
     VADD.F32 s5, s4, s5        // calculate p + r, result in s5
     VDIV.F32 s5, s4, s5        // calculate k = p / (p+r) , result in s5
     VSTR.32 s5, [r0, #16]    // save new k in struct
 
-    // calculate x
-    VLDR.32 s4, [r0, #8]    // load x in s4
-    VSUB.F32 s5, s0, s4        // calculate measurement - x, result in s5
-    VLDR.32 s6, [r0, #16]    // load k in s6
-    VMUL.F32 s5, s6, s5        // calculate k * (measurement-x), result in s5
-    VADD.F32 s4, s4, s5        // calculate x + s5, result in s4
-    VSTR.32 s4, [r0, #8]    // save new x in struct
+	// p in S4   k in S5
+    // calculate x = x + k * (meas - x)
+    VLDR.32 s6, [r0, #8]    // load x in s6
+    VSUB.F32 s7, s0, s6		// calculate meas - x, result in s7
+    VMLA.32 s6, s5, s7		// calculate x, result in s6
+    VSTR.32 s6, [r0, #8]	// save new x in struct
 
-    // calculate p again
-
-    VMOV.F32 S4, #1.0   // load 1.0 in s4
-    VLDR.32 s5, [r0, #16]   // load k in s5
-    VSUB.F32 s5, s4, s5        // 1.0 - k, result in s5
-    VLDR.32 s4, [r0, #12]     // load p in s4
-    VMUL.F32 s4, s5, s4        // p = (1-k) * p, result in s4
+    // calculate p = (1 - k) * p
+    VMOV.F32 s7, #1.0   // load 1.0 in s7
+    VSUB.F32 s5, s7, s5		// calculate 1 - k, result in s5
+    VMUL.F32 s4, s5, s4		// calculate new p, result in s4
     VSTR.32 s4, [r0, #12]    // save new p in struct
 
-    VPOP.32 {s4, s5, s6}
+    VPOP.32 {s4, s5, s6, s7}
     BX LR
